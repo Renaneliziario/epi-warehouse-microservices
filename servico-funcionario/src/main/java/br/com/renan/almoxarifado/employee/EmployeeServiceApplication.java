13d5a01b6d136ad0d876b95bb8f173b58f4f2c32
Renaneliziario <renanqueiroz92@gmail.com>
2026-08-08 11:16:26 -0300

    chore: adicionar bootstrap dos tres servicos de dominio com cliente config

---
+++ servico-funcionario/src/main/java/br/com/renan/almoxarifado/employee/EmployeeServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.renan.almoxarifado.employee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeServiceApplication.class, args);
+    }
+}
---
+++ servico-retirada-epi/src/main/java/br/com/renan/almoxarifado/epiwithdrawal/EpiWithdrawalServiceApplication.java
@@ -0,0 +1,12 @@
+package br.com.renan.almoxarifado.epiwithdrawal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpiWithdrawalServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpiWithdrawalServiceApplication.class, args);
+    }
+}
